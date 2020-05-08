5ee782b635f1922128b1014225eac5cac56ebe73
Tareq Ahmed <tareq57ahmed@gmail.com>
2020-05-08 18:32:00 +0600

    Add solution for problem `A - Helpful Maths` Link: https://codeforces.com/contest/339/problem/A Tags: greedy, implementation, sortings, strings, *800 Ref: https://codeforces.com/contest/339/submission/79377756

---
+++ codeforces/339/A.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define ull  unsigned long long
+#define vll  vector <long long>
+#define pll pair <long long,long long>
+#define f first
+#define s second
+#define up upper_bound
+#define lp lower_bound
+#define pq priority_queue
+#define inf 1e10
+#define minf -1e10
+#define pi 3.14159265
+#define mod 1000000000
+#define fastio ios_base::sync_with_stdio(0);cin.tie(NULL);
+int main()
+{
+    string str;
+    cin>>str;
+    vector <char> vec;
+    for(ll i=0;i<str.size();i++)
+    {
+        if(str[i]!='+')
+        {
+            vec.pb(str[i]);
+        }
+    }
+    sort(vec.begin(),vec.end());
+    cout<<vec[0];
+    for(ll i=1;i<vec.size();i++)
+    {
+        cout<<"+"<<vec[i];
+    }
+
+}
