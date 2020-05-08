49910a889713a22099e47be6bfd114ee39a4fa63
Tareq Ahmed <tareq57ahmed@gmail.com>
2020-05-08 07:23:00 +0600

    Add solution for problem `A - Almost Prime` Link: https://codeforces.com/contest/26/problem/A Tags: number theory, *900 Ref: https://codeforces.com/contest/26/submission/79336002

---
+++ codeforces/26/A.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define ull  unsigned long long
+#define vll  vector <long long>
+#define pll pair <long long,long long>
+#define f first
+#define s second
+#define up upper_bound
+#define lp lower_bound
+#define pq priority_queue
+#define inf 1e10
+#define minf -1e10
+#define pi 3.14159265
+#define mod 1000000000
+#define fastio ios_base::sync_with_stdio(0);cin.tie(NULL);
+int main()
+{
+  ll n;
+  cin>>n;
+  ll ans=0;
+  set <ll> s;
+  for(ll i=6;i<=n;i++)
+  {
+      ll p=i;
+      for(ll j=2;j<=p;j++)
+      {
+          if(p%j==0)
+          {
+              while(p%j==0)
+              {
+                  p/=j;
+              }
+              s.insert(j);
+          }
+      }
+      if(s.size()==2)
+        ans++;
+      s.clear();
+  }
+  cout<<ans<<endl;
+}
