683c43ea6fd5deaead587e1e53de4086b90fe59e
Tareq Ahmed <tareq57ahmed@gmail.com>
2020-08-09 20:47:00 +0600

    Add solution for problem `B - Fix You` Link: https://codeforces.com/contest/1391/problem/B Tags: brute force, greedy, implementation, *800 Ref: https://codeforces.com/contest/1391/submission/89420989

---
+++ codeforces/1391/B.cpp
@@ -0,0 +1,39 @@
+//BISMILLAH
+//RABBI JIDNI ILMA
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define ull  unsigned long long
+#define vll  vector <long long>
+#define pll pair <long long,long long>
+#define f first
+#define s second
+#define up upper_bound
+#define lp lower_bound
+#define pq priority_queue
+#define inf 1e10
+#define minf -1e10
+#define pi 3.14159265
+#define mod 1000000000
+#define fastio ios_base::sync_with_stdio(0);cin.tie(NULL);
+#define rep(p,q) for(ll i=p;i<q;i++)
+#define per(p,q) for(ll i=p-1;i>=0;i--)
+#define o 0
+int main()
+{
+    fastio
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n,m;
+        cin>>n>>m;
+        char ara[101][101];
+        for(ll i=1;i<=n;i++)for(ll j=1;j<=m;j++)cin>>ara[i][j];ll cnt=0;
+        for(ll i=1;i<n;i++)if(ara[i][m]=='R')cnt++;
+        for(ll i=1;i<m;i++)if(ara[n][i]=='D')cnt++;
+        cout<<cnt<<endl;
+    }
+}
