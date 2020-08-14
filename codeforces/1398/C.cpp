3b7bf80d2def135bba72e8b96206909d18ff7c37
Tareq Ahmed <tareq57ahmed@gmail.com>
2020-08-14 22:55:00 +0600

    Add solution for problem `C - Good Subarrays` Link: https://codeforces.com/contest/1398/problem/C Tags: data structures, dp, math, *1600 Ref: https://codeforces.com/contest/1398/submission/89957777

--- codeforces/1398/C.cpp
+++ codeforces/1398/C.cpp
@@ -20,27 +20,62 @@ using namespace std;
 #define fastio ios_base::sync_with_stdio(0);cin.tie(NULL);
 #define rep(p,q) for(ll i=p;i<q;i++)
 #define per(p,q) for(ll i=p-1;i>=0;i--)
+
+ll findSubarraySum(ll arr[], ll n, ll sum) 
+{ 
+    // STL map to store number of subarrays 
+    // starting from index zero having  
+    // particular value of sum. 
+    unordered_map<ll, ll> prevSum; 
+  
+    ll res = 0; 
+  
+    // Sum of elements so far. 
+    ll currsum = 0; 
+  
+    for (ll i = 0; i < n; i++) { 
+  
+        // Add current element to sum so far. 
+        currsum += arr[i]; 
+  
+        // If currsum is equal to desired sum, 
+        // then a new subarray is found. So 
+        // increase count of subarrays. 
+        if (currsum == sum)  
+            res++;         
+  
+        // currsum exceeds given sum by currsum  
+        //  - sum. Find number of subarrays having  
+        // this sum and exclude those subarrays 
+        // from currsum by increasing count by  
+        // same amount. 
+        if (prevSum.find(currsum - sum) !=  
+                                  prevSum.end())  
+            res += (prevSum[currsum - sum]); 
+          
+  
+        // Add currsum value to count of  
+        // different values of sum. 
+        prevSum[currsum]++; 
+    } 
+  
+    return res; 
+} 
+ 
 int main()
 {
    fastio
-   ll t;
+   ll t,w;
    cin>>t;
    while(t--)
    {
-          ll n;
-   cin>>n;
-   string str;
-   cin>>str;
-   ll sum=0,ans=0;
-   map<ll,ll>mapp;
-   mapp[0]=1;
-   for(ll i=0;i<n;i++)
-   {
-       ll p=str[i]-'0'-1;
-       sum+=p;
-       ans+=mapp[sum];
-       mapp[sum]++;
-   }
-   cout<<ans<<endl;
+       ll n;string str;
+       cin>>n>>str;
+       ll ara[100005];
+       for(ll i=0;i<n;i++)
+       {
+           ara[i]=str[i]-'0'-1;
+       }
+       cout<<findSubarraySum(ara,n,0)<<endl;
    }
 }
