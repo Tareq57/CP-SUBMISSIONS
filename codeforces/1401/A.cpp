fe1daf69a530afa66c8424a402a673053bb400a9
Tareq Ahmed <tareq57ahmed@gmail.com>
2020-08-22 00:59:00 +0600

    Add solution for problem `A - Distance and Axis` Link: https://codeforces.com/contest/1401/problem/A Tags: constructive algorithms, math, *900 Ref: https://codeforces.com/contest/1401/submission/90631287

---
+++ codeforces/1401/A.cpp
@@ -0,0 +1,46 @@
+
+//BISMILLAH
+//RABBI JIDNI ILMA
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define ull  unsigned long long
+#define vll  vector <long long>
+#define pll pair <long long,long long>
+#define f first
+#define s second
+#define up upper_bound
+#define lp lower_bound
+#define pq priority_queue
+#define inf 1e10
+#define minf -1e10
+#define pi 3.14159265
+#define mod 1000000007
+#define fastio ios_base::sync_with_stdio(0);cin.tie(NULL);
+#define rep(p,q) for(ll i=p;i<q;i++)
+#define per(p,q) for(ll i=p-1;i>=0;i--)
+int main()
+{
+   fastio
+   ll t;
+   cin>>t;
+   while(t--)
+   {
+       ll n,k;
+       cin>>n>>k;
+       if(n<k)
+       cout<<abs(n-k)<<endl;
+       else
+       {
+           if(n%2==k%2)
+            cout<<0<<endl;
+           else
+            cout<<1<<endl;
+       }
+   }
+ 
+ 
+}
+ 
