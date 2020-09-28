d711abc0f3bb9cfa12b740a9ed6e6f743e3d0525
Tareq Ahmed <tareq57ahmed@gmail.com>
2020-09-28 15:03:00 +0600

    Add solution for problem `C - Increase and Copy` Link: https://codeforces.com/contest/1426/problem/C Tags: binary search, constructive algorithms, math, *1100 Ref: https://codeforces.com/contest/1426/submission/94091509

---
+++ codeforces/1426/C.cpp
@@ -0,0 +1,39 @@
+//BISMILLAH
+//RABBI JIDNI ILMA
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define ull  unsigned long long
+#define vll  vector <long long>
+#define pll pair <long long,long long>
+#define f first
+#define s second
+#define up upper_bound
+#define lp lower_bound
+#define pq priority_queue
+#define inf 1e10
+#define minf -1e10
+#define pi 3.14159265
+#define mod 1000000007
+#define fastio ios_base::sync_with_stdio(0);cin.tie(NULL);
+int main()
+{
+    fastio
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+       ll n;
+       cin>>n;
+       ll p=(ll)sqrt(n);
+       ll res=p;
+       while(p*res<n)
+       {
+           p++;
+       }
+       res+=(p-1);
+       cout<<res-1<<endl;
+    }
+}
