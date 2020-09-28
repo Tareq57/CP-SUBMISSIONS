86473c8504a8ab307f6e134dab3f5b72289a65d3
Tareq Ahmed <tareq57ahmed@gmail.com>
2020-09-28 14:07:00 +0600

    Add solution for problem `A - Floor Number` Link: https://codeforces.com/contest/1426/problem/A Tags: implementation, math, *800 Ref: https://codeforces.com/contest/1426/submission/94061234

---
+++ codeforces/1426/A.cpp
@@ -0,0 +1,38 @@
+//BISMILLAH
+//RABBI JIDNI ILMA
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define ull  unsigned long long
+#define vll  vector <long long>
+#define pll pair <long long,long long>
+#define f first
+#define s second
+#define up upper_bound
+#define lp lower_bound
+#define pq priority_queue
+#define inf 1e10
+#define minf -1e18
+#define pi 3.14159265
+#define mod 1000000007
+#define fastio ios_base::sync_with_stdio(0);cin.tie(NULL);
+int main()
+{
+    fastio
+    ll t;
+    cin>>t;
+    while(t--)
+    {
+        ll n,x;
+        cin>>n>>x;
+        ll p=2,ans=1;
+        while(p<n)
+        {
+            p+=x;
+            ans++;
+        }
+        cout<<ans<<endl;
+    }
+}
