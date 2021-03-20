6c72b656ab06aeb08f577c1d88f25440b50a22f4
Tareq Ahmed <tareq57ahmed@gmail.com>
2021-03-21 04:04:00 +0600

    Add solution for problem `C1 - Good Numbers (easy version)` Link: https://codeforces.com/contest/1249/problem/C1 Tags: brute force, greedy, implementation, *1300 Ref: https://codeforces.com/contest/1249/submission/110574557

--- codeforces/1249/C1.cpp
+++ codeforces/1249/C1.cpp
@@ -25,71 +25,92 @@ ll ceil(ll p,ll q)
     r++;
     return r;
 }
+set<ll>s;
+void elem()
+{
+    for(ll i=0;i<10;i++)
+    {
+        s.insert((ll)pow(3,i));
+    }
+    for(ll i=0;i<9;i++)
+        for(ll j=i+1;j<10;j++)
+            s.insert(pow(3,i)+pow(3,j));
+    for(ll i=0;i<8;i++)
+        for(ll j=i+1;j<9;j++)
+            for(ll k=j+1;k<10;k++)
+                s.insert(pow(3,i)+pow(3,j)+pow(3,k));
+    for(ll i=0;i<7;i++)
+        for(ll j=i+1;j<8;j++)
+            for(ll k=j+1;k<9;k++)
+                for(ll l=k+1;l<10;l++)
+                   s.insert(pow(3,i)+pow(3,j)+pow(3,k)+pow(3,l));
+    
+     for(ll i=0;i<6;i++)
+        for(ll j=i+1;j<7;j++)
+            for(ll k=j+1;k<8;k++)
+                for(ll l=k+1;l<9;l++)
+                    for(ll m=l+1;m<10;m++)
+                        s.insert(pow(3,i)+pow(3,j)+pow(3,k)+pow(3,l)+pow(3,m));
+  
+     for(ll i=0;i<5;i++)
+        for(ll j=i+1;j<6;j++)
+            for(ll k=j+1;k<7;k++)
+                for(ll l=k+1;l<8;l++)
+                    for(ll m=l+1;m<9;m++)
+                        for(ll n=m+1;n<10;n++)
+                          s.insert(pow(3,i)+pow(3,j)+pow(3,k)+pow(3,l)+pow(3,m)+pow(3,n));
+    for(ll i=0;i<4;i++)
+        for(ll j=i+1;j<5;j++)
+            for(ll k=j+1;k<6;k++)
+                for(ll l=k+1;l<7;l++)
+                    for(ll m=l+1;m<8;m++)
+                        for(ll n=m+1;n<9;n++)
+                            for(ll o=n+1;o<10;o++)
+                                    s.insert(pow(3,i)+pow(3,j)+pow(3,k)+pow(3,l)+pow(3,m)+pow(3,n)+pow(3,o));
+    for(ll i=0;i<3;i++)
+        for(ll j=i+1;j<4;j++)
+            for(ll k=j+1;k<5;k++)
+                for(ll l=k+1;l<6;l++)
+                    for(ll m=l+1;m<7;m++)
+                        for(ll n=m+1;n<8;n++)
+                            for(ll o=n+1;o<9;o++)
+                                for(ll p=o+1;p<10;p++)
+                                    s.insert(pow(3,i)+pow(3,j)+pow(3,k)+pow(3,l)+pow(3,m)+pow(3,n)+pow(3,o)+pow(3,p));
+    for(ll i=0;i<2;i++)
+        for(ll j=i+1;j<3;j++)
+            for(ll k=j+1;k<4;k++)
+                for(ll l=k+1;l<5;l++)
+                    for(ll m=l+1;m<6;m++)
+                        for(ll n=m+1;n<7;n++)
+                            for(ll o=n+1;o<8;o++)
+                                for(ll p=o+1;p<9;p++)
+                                    for(ll q=p+1;q<10;q++)
+                                        s.insert(pow(3,i)+pow(3,j)+pow(3,k)+pow(3,l)+pow(3,m)+pow(3,n)+pow(3,o)+pow(3,p)+pow(3,q));
+    ll sum=0;
+    for(ll i=0;i<10;i++)
+    {
+        sum+=(pow(3,i));
+    }
+    s.insert(sum);
+}
+
 int main()
 {
    ll t;
    cin>>t;
+   elem();
    while(t--)
    {
-       ll n;
-       cin>>n;
-       vector<ll> vec;
-       while(n>0)
-       {
-           ll p=n%3;
-           vec.pb(p);
-           n/=3;
-       } 
-      // reverse(vec.begin(),vec.end());
-       ll res=0;
-       set<ll>zero,two;
-       for(ll i=0;i<vec.size();i++)
-       {
-           if(vec[i]==0)
-               zero.insert(i);
-           else if(vec[i]==2)
-               two.insert(i);
-       }
-       while(!two.empty())
-       {
-           auto i=*two.begin();
-           two.erase(i);
-           auto it=up(zero.begin(),zero.end(),i);
-           if(zero.end()==it)
-           {
-               for(ll j=0;j<vec.size();j++)
-               {
-                   vec[j]=0;
-                   zero.insert(j);
-               }
-               vec.pb(1);
-               break;
-           }
-           else
-           {
-               vec[*it]=1;
-               for(ll j=0;j<*it;j++)
-               {
-                   if(vec[j]==2)
-                   {
-                           two.erase(j);
-                   }
-                   vec[j]=0;
-                   zero.insert(j);
-               }
-           }
-       }
-       for(ll i=0;i<vec.size();i++)
-       {
-           ll p=1;
-           for(ll j=0;j<i;j++)
-           {
-               p*=3;
-           }
-           res+=(vec[i]*p);
-       }
-       cout<<res<<endl;
+      ll n;
+      cin>>n;
+      auto it=up(s.begin(),s.end(),n);
+      if(*(--it)==n)
+      {
 
+      }
+      else
+      it++;
+      cout<<*it<<endl;
    }
      
 }
