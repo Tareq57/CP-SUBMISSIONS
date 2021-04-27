5e777c781d1bb997c34899a21e65ddbda3506ead
Tareq Ahmed <tareq57ahmed@gmail.com>
2021-04-27 06:21:00 +0600

    Add solution for problem `C - Fillomino 2` Link: https://codeforces.com/contest/1517/problem/C Tags: constructive algorithms, dfs and similar, greedy, implementation, *1400 Ref: https://codeforces.com/contest/1517/submission/114320416

---
+++ codeforces/1517/C.cpp
@@ -0,0 +1,165 @@
+//BISMILLAH
+//RABBI JIDNI ILMA
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define ull  unsigned long long
+#define vll  vector <long long>
+#define pll pair <long long,long long>
+#define f first
+#define s second
+#define up upper_bound
+#define lp lower_bound
+#define pq priority_queue
+#define inf 1e10
+#define minf -1e15
+#define pi 3.14159265
+#define mod 1000000007
+#define fastio ios_base::sync_with_stdio(0);cin.tie(NULL);
+#define N 1000000
+bool marked[N];
+bool isPrime(int n) {
+  if (n < 2) return false;
+  for (long long i = 2; i * i <= n; i++) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+vll sieve(int n) {
+    vll vec;
+  for (int i = 2; i * i <= n; i++) {
+    if (marked[i] == false) { 
+      for (int j = i * i; j <= n; j += i) {
+        marked[j] = true;
+      }
+    }
+  }
+  for(ll i=1;i<=n;i++)
+  {
+      if(marked[i]==false)
+      {
+          vec.pb(i);
+      }
+  }
+  return vec;
+}
+ll countDivisor(ll n) {
+  ll divisor = 0;
+  for (ll i = 1; i * i <= n; i++) {
+    if (i * i == n){
+      divisor += 1;
+    }else if (n % i == 0) {
+      divisor += 2;
+      
+    }
+  }
+  return divisor;
+  
+}
+set<ll> prime_factors(ll n)
+{
+  set<ll> ss;
+  for(ll i=2;i*i<=n;i++)
+  {
+    if(n%i==0)
+    {
+      while(n%i==0)
+      n/=i;
+      ss.insert(i);
+    }
+  }
+  if(n>1)ss.insert(n);
+  return ss;
+}
+ll euler_phi (ll n) {
+  ll ret = n;
+  for (ll i = 2; i * i <= n; i++) {
+    if (n % i == 0) {
+      while (n % i == 0) {
+        n /= i;
+      }
+      ret -= ret / i; 
+    }
+  }
+  if (n > 1) ret -= ret / n;
+  return ret;
+}
+pll extendedEuclid(ll a, ll b) {
+  if(b == 0) return pll(1, 0);
+  else {
+    pll d = extendedEuclid(b, a % b);
+    return pll(d.s, d.f - d.s * (a / b));
+  }
+}
+ll modularInverse(ll a, ll n) {
+  pll ret = extendedEuclid(a, n);
+  return ((ret.f % n) + n) % n;
+}
+ll bigmod(ll a, ll b, ll M)
+{
+    if(b == 0) return 1 % M;
+    ll x = bigmod(a, b/2, M);
+    x = (x*x)%M;
+    if(b%2 == 1) x = (x*a)%M;
+    return x;
+}
+vll power(ll n,ll p)
+{
+  ll pow=1;
+  vll vec;
+  for(ll i=0;i<p;i++)
+  {
+    vec.pb(pow);
+    pow*=n;
+  }
+  return vec;
+
+}
+int main()
+{
+  fastio
+  
+    ll n;
+    cin>>n;
+    ll ara[n+1],left[n+1];
+    ll res[n+1][n+1]={0};
+    for(ll i=1;i<=n;i++)
+    {
+      for(ll j=1;j<=n;j++)
+      {
+        if(j<i)
+        res[i][j]=0;
+        else if(i==j)res[i][j]=ara[i];
+        else
+        res[i][j]=inf;
+      }
+    }
+    for(ll i=1;i<=n;i++)
+    {
+      cin>>ara[i];
+      ll x=i,y=i;
+      ll cnt=ara[i];
+      while(cnt)
+      {
+        res[x][y]=ara[i];
+        cnt--;
+        if(y-1>0&&res[x][y-1]==0)
+        y-=1;
+        else
+        x++;
+      }
+    }
+    for(ll i=1;i<=n;i++)
+    {
+      for(ll j=1;j<=i;j++)
+      cout<<res[i][j]<<" ";
+      cout<<endl;
+    }
+  
+
+
+}
